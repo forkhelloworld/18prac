24a2595123b6355b2bd946df2267e5b566d09f10
forkhelloworld <serhiisliusarskyi@gmail.com>
2024-07-06 13:31:49 +0300

    Add some operations with primitive methods and error processing

---
+++ index.js
@@ -0,0 +1,37 @@
+let str = "qwerty";
+str = str.split("").reverse().join("");
+
+function integer(num) {
+  return Math.trunc(num);
+}
+
+function capsName(name) {
+  return name.toUpperCase();
+}
+
+function reformatDate(dateStr) {
+  let parts = dateStr.split("-");
+
+  let formattedDate = `${parts[1]}.${parts[2]}.${parts[0]}`;
+
+  return formattedDate;
+}
+
+function isEqual(...str) {
+  for (const string of str) {
+    if (str[0].toLowerCase() !== string.toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function errorFun() {
+  return a;
+}
+
+try {
+    errorFun()
+} catch (error) {
+    console.log('error :>> ', error);
+}
